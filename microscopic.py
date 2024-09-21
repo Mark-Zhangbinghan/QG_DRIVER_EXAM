7a681f987cd2122bccbb7d175b423d9d5fe21e52
as <2446811412@qq.com>
2024-09-21 13:18:19 +0800

    refactor - 去除后台运行逻辑

--- microscopic.py
+++ microscopic.py
@@ -19,15 +19,15 @@ executor = concurrent.futures.ThreadPoolExecutor()
 @micro_app.put("/put_sub_position")
 async def post_sub_position(path_request: Request, background_tasks: BackgroundTasks):
     path_json = await path_request.json()
-    background_tasks.add_task(process_data, path_json)
-    return {"message": "数据处理已开始在后台执行"}
-
-
-async def process_data(path_json):
     loop = asyncio.get_event_loop()
     # 在单独的线程中运行耗时的计算任务
-    await loop.run_in_executor(executor, get_data, path_json)
-    print("数据处理完成")
+    share.array_list = await loop.run_in_executor(executor, get_data, path_json)
+    return {"put succeed"}
+# async def process_data(path_json):
+#     loop = asyncio.get_event_loop()
+#     # 在单独的线程中运行耗时的计算任务
+#     await loop.run_in_executor(executor, get_data, path_json)
+#     print("数据处理完成")
 
 
 @micro_app.get("/get_sub_position")
