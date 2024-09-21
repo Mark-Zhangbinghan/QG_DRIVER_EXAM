26f48c39a3a5dbe95c05c62a6c43eaf20ccfe1a6
as <2446811412@qq.com>
2024-09-21 12:27:27 +0800

    refactor - 实现池化异步操作

--- microscopic.py
+++ microscopic.py
@@ -1,10 +1,11 @@
 import asyncio
-from fastapi import FastAPI, File, UploadFile, WebSocket
+from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi import Request
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import json
 import numpy as np
+import concurrent.futures
 
 # 自定义引用
 from CAV.code.position_give import get_data
@@ -12,13 +13,21 @@ from add_json import sub_switch_road
 import share
 
 micro_app = FastAPI()
+executor = concurrent.futures.ThreadPoolExecutor()
 
 
 @micro_app.put("/put_sub_position")
-async def post_sub_position(path_request: Request):
+async def post_sub_position(path_request: Request, background_tasks: BackgroundTasks):
     path_json = await path_request.json()
-    share.array_list = get_data(path_json)
-    return {"put succeed"}
+    background_tasks.add_task(process_data, path_json)
+    return {"message": "数据处理已开始在后台执行"}
+
+
+async def process_data(path_json):
+    loop = asyncio.get_event_loop()
+    # 在单独的线程中运行耗时的计算任务
+    await loop.run_in_executor(executor, get_data, path_json)
+    print("数据处理完成")
 
 
 @micro_app.get("/get_sub_position")
